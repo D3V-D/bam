972b268d0487b53098a4b6243743008de32827df
D3V-D <devendangi10@gmail.com>
2023-06-20 22:13:45 -0400

    include other files

---
+++ js/scrollText.js
@@ -0,0 +1,43 @@
+//appear letters as you scroll down
+let previousY = window.scrollY;
+window.onscroll = function (e) {
+
+    const letters = document.getElementsByClassName("letter");
+    const phrases = document.getElementsByClassName("phrase");
+    let i = 1;
+    for (i = 1; i < letters.length + 1; i++) {
+        
+        // hide first phrase (6 letters) when done, and change colors
+        if (window.scrollY >= window.innerHeight + (150 * 6)) {
+            phrases[0].classList.add("hidden");
+            if (window.scrollY < window.innerHeight + (150 * (10+6))) {
+                document.getElementById("phraseContainer").style.color = "white";
+                document.getElementById("phraseContainer").style.background = "black";
+            }
+        } else {
+            phrases[0].classList.remove("hidden");
+            document.getElementById("phraseContainer").style.color = "black";
+            document.getElementById("phraseContainer").style.background = "white";
+        }
+        
+        // similar to previous section, but for third phrase to appear
+        if (window.scrollY >= window.innerHeight + (150 * (10 + 6))) {
+            phrases[1].classList.add("hidden");
+            document.getElementById("phraseContainer").style.color = "black";
+            document.getElementById("phraseContainer").style.background = "white";
+        } else if (window.scrollY > window.innerHeight + (150 * 6)) {
+            phrases[1].classList.remove("hidden");
+            document.getElementById("phraseContainer").style.color = "white";
+            document.getElementById("phraseContainer").style.background = "black";
+        }
+
+        
+        // show letters && hide letters
+        if (window.scrollY > window.innerHeight + (150 * i)) {
+            letters[i-1].classList.remove("hidden");
+        } else {
+            letters[i-1].classList.add("hidden");
+        }
+    }
+    previousY = window.scrollY;
+}
