2e43c13dc5006ed8957fed94f65407b0098c14f9
D3V-D <devendangi10@gmail.com>
2023-06-21 20:16:54 -0400

    added hamburger menu

--- js/scrollText.js
+++ js/scrollText.js
@@ -8,9 +8,9 @@ window.onscroll = function (e) {
     for (i = 1; i < letters.length + 1; i++) {
         
         // hide first phrase (6 letters) when done, and change colors
-        if (window.scrollY >= window.innerHeight + (150 * 6)) {
+        if (window.scrollY >= window.innerHeight + (150 * 5)) {
             phrases[0].classList.add("hidden");
-            if (window.scrollY < window.innerHeight + (150 * (10+6))) {
+            if (window.scrollY < window.innerHeight + (150 * (10+5))) {
                 document.getElementById("phraseContainer").style.color = "white";
                 document.getElementById("phraseContainer").style.background = "black";
             }
@@ -21,11 +21,11 @@ window.onscroll = function (e) {
         }
         
         // similar to previous section, but for third phrase to appear
-        if (window.scrollY >= window.innerHeight + (150 * (10 + 6))) {
+        if (window.scrollY >= window.innerHeight + (150 * (10 + 5))) {
             phrases[1].classList.add("hidden");
             document.getElementById("phraseContainer").style.color = "black";
             document.getElementById("phraseContainer").style.background = "white";
-        } else if (window.scrollY > window.innerHeight + (150 * 6)) {
+        } else if (window.scrollY > window.innerHeight + (150 * 5)) {
             phrases[1].classList.remove("hidden");
             document.getElementById("phraseContainer").style.color = "white";
             document.getElementById("phraseContainer").style.background = "black";
@@ -33,7 +33,7 @@ window.onscroll = function (e) {
 
         
         // show letters && hide letters
-        if (window.scrollY > window.innerHeight + (150 * i)) {
+        if (window.scrollY > window.innerHeight + (150 * (i-1)) - 2) {
             letters[i-1].classList.remove("hidden");
         } else {
             letters[i-1].classList.add("hidden");
