52784c588471e7fb535f2192b29eff48ee3f43fc
D3V-D <devendangi10@gmail.com>
2023-06-26 01:01:30 -0400

    Added torrent-of-info section. Still WIP.

---
+++ js/torrent.js
@@ -0,0 +1,15 @@
+//change as needed
+const numOfTorrentImages = 40;
+for (let i = 1; i <= numOfTorrentImages; i++) {
+    // loop through and create img html for each image
+    document.getElementById("torrent-container").innerHTML += "<img src='./public/mazeImgs/newsimage" + i + ".jpeg' class='torrent-image' id='torrent-image-" + i + "'>"
+}
+
+const torrentContainer = document.querySelector('#torrent-container');
+const torrentImages = document.querySelectorAll('.torrent-image');
+
+// Randomize flow times and positions for each image
+torrentImages.forEach((image) => {
+  image.style.top = (Math.random() * 90) + "%"
+  image.style.animation = "image-flow " + ((Math.random() * 5) + 2) + "s linear infinite"
+});
