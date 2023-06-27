7f6b97fc59c3b80fc46a39f8d572cdf9a2570c72
D3V-D <devendangi10@gmail.com>
2023-06-26 22:44:16 -0400

    further edits

--- js/scrollText.js
+++ js/scrollText.js
@@ -5,7 +5,7 @@ const sections = document.querySelectorAll('header, section, footer')
 
 window.onscroll = function (e) {
     sections.forEach(section => {
-        if (window.pageYOffset > section.offsetTop - section.offsetHeight/2) {
+        if (window.pageYOffset > section.offsetTop - (section.offsetHeight/3)) {
             currSection = section
         }
     })
